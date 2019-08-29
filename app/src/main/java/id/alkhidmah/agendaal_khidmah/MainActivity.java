773fd61e502a69aa037a206d8de63cc7402a7b33
dzakybd <dzakybd@gmail.com>
2019-08-29 12:26:35 +0900

    Initial commit

---
+++ app/src/main/java/id/alkhidmah/agendaal_khidmah/MainActivity.java
@@ -0,0 +1,14 @@
+package id.alkhidmah.agendaal_khidmah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
