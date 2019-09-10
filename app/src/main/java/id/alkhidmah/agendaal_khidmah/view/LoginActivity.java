dcac280b2de32d3d798c808aec18bfaf1a411154
dzakyzf <dzakybd@gmail.com>
2019-09-11 08:47:13 +0900

    commit

--- app/src/main/java/id/alkhidmah/agendaal_khidmah/view/LoginActivity.java
+++ app/src/main/java/id/alkhidmah/agendaal_khidmah/view/LoginActivity.java
@@ -1,16 +1,44 @@
 package id.alkhidmah.agendaal_khidmah.view;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+
+import java.util.List;
 
 import id.alkhidmah.agendaal_khidmah.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements Validator.ValidationListener {
+
+    private EditText mFieldakun;
+    private EditText mFieldpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mFieldakun = findViewById(R.id.fieldakun);
+        mFieldpassword = findViewById(R.id.fieldpassword);
+    }
+
+    public void masukclick(View view) {
+    }
+
+    public void daftarclick(View view) {
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+
     }
 }
